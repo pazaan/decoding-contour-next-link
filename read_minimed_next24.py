4bfd86ff9d4b35ca47a8c0377f09ccc608e30280
paulokow <31203062+paulokow@users.noreply.github.com>
2017-08-30 12:26:40 +0200

    Date and logger added to logging config

--- read_minimed_next24.py
+++ read_minimed_next24.py
@@ -2,7 +2,7 @@
 
 import logging
 #logging.basicConfig has to be before astm import, otherwise logs don't appear
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s [%(name)s] %(message)s', level=logging.DEBUG)
 #just to avoid flooding events from transitions module
 logging.getLogger('transitions').setLevel(logging.WARNING)
 # a nasty workaround on missing hidapi.dll on my windows (allows testing from saved files, but not download of pump)
