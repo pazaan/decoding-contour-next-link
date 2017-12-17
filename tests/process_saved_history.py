654e7105f78819db3a8c7c09cf0e207fd6c0e245
paulokow <31203062+paulokow@users.noreply.github.com>
2017-12-17 23:42:30 +0100

    TEST: testing duplicated events

--- tests/process_saved_history.py
+++ tests/process_saved_history.py
@@ -1,14 +1,35 @@
 import pickle
 import decoding_contour_next_link
 from decoding_contour_next_link import HISTORY_DATA_TYPE
+from datetime import tzinfo, timedelta, datetime
+
+ZERO = timedelta(0)
+HOUR = timedelta(hours=1)
+
+# A UTC class.
+
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        return ZERO
+
+utc = UTC()
 
 if __name__ == '__main__':
     history_pages = None
-    with open('../testdata/paulokow_20170827_sample.dat', 'rb') as input_file:
+    with open('../testdata/paulokow_20171217_cgm_sample.dat', 'rb') as input_file:
         history_pages = pickle.load(input_file)
     mt = decoding_contour_next_link.Medtronic600SeriesDriver()
-    events = mt.processPumpHistory(history_pages, HISTORY_DATA_TYPE.PUMP_DATA)
-    print "# All events:"
+    events = mt.processPumpHistory(history_pages, HISTORY_DATA_TYPE.SENSOR_DATA)
+    print ("# All events:")
     for ev in events:
-        print ev
-    print "# End events"
+        if ev.timestamp > datetime(2017, 12, 14, 19, 0, 0, 0, tzinfo=utc) and ev.timestamp < datetime(2017, 12, 14, 20, 0, 0, 0, tzinfo=utc): 
+            print (ev)
+    print ("# End events")
